f0ac9616d0cc9bbddc9a7fc112ab0503b611b5f0
Nexterr <68924751+Nexterr@users.noreply.github.com>
2020-10-15 21:38:54 +0300

    function PlayAddressT for desc_html

--- youtube.lua
+++ youtube.lua
@@ -1,4 +1,4 @@
--- видеоскрипт для сайта https://www.youtube.com (15/10/20)
+-- видеоскрипт для сайта https://www.youtube.com (16/10/20)
 --[[
 	Copyright © 2017-2020 Nexterr
 	Licensed under the Apache License, Version 2.0 (the "License");
@@ -971,8 +971,8 @@ https://github.com/grafi-tt/lunaJson
 			desc = desc:gsub('"+', '"')
 			desc = desc:gsub('(<a href=".-)%)"', '%1"')
 			desc = desc:gsub('%)</a>', '</a>%)')
-			desc = desc:gsub('none">(https?://[%a%.]*youtu[%.combe].-)</a>', 'none">%1</a> <a href="simpleTVLua:m_simpleTV.Control.PlayAddress(\'%1\')"><img src="https://raw.githubusercontent.com/Nexterr/simpleTV.img/master/YT_play.png" height="32" valign="top"></a>')
-			desc = desc:gsub('none">(https?://[%w%.]*twitch%.tv.-)</a>', 'none">%1</a> <a href="simpleTVLua:m_simpleTV.Control.PlayAddress(\'%1\')"><img src="https://raw.githubusercontent.com/Nexterr/simpleTV.img/master/YT_play.png" height="32" valign="top"></a>')
+			desc = desc:gsub('none">(https?://[%a%.]*youtu[%.combe].-)</a>', 'none">%1</a> <a href="simpleTVLua:PlayAddressT(\'%1\')"><img src="https://raw.githubusercontent.com/Nexterr/simpleTV.img/master/YT_play.png" height="32" valign="top"></a>')
+			desc = desc:gsub('none">(https?://[%w%.]*twitch%.tv.-)</a>', 'none">%1</a> <a href="simpleTVLua:PlayAddressT(\'%1\')"><img src="https://raw.githubusercontent.com/Nexterr/simpleTV.img/master/YT_play.png" height="32" valign="top"></a>')
 				for t0, t in desc:gmatch('(.)#(%S+)') do
 					t = t:gsub('%p*$', '')
 					if not t:match('^%d%d?$') and (t0 == ' ' or t0 == '\n') then
@@ -2661,6 +2661,9 @@ https://github.com/grafi-tt/lunaJson
 			m_simpleTV.PositionThumbs.AppendThumb(t)
 		 return true
 		end
+	end
+	function PlayAddressT(address)
+		m_simpleTV.Control.PlayAddressT({address = address})
 	end
 	function SavePlst_YT()
 		if m_simpleTV.User.YT.Plst and m_simpleTV.User.YT.plstHeader then
